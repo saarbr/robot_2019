c61686a66e994cf63ee436659b741aed8371088b
saarbr <55541462+saarbr@users.noreply.github.com>
2020-05-04 19:13:25 +0300

    Add files via upload

---
+++ commands/climber/Climber_command.java
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Subsystem_climber;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class Climber_command extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Subsystem_climber m_subsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem 
+   */
+  public Climber_command() {
+    subsystem_climber = Subsystem_climber.getinstance();
+    
+    addRequirements(Subsystem_climber);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+        if (Subsystem_climber.get() == Value.kForward){
+
+        Subsystem_climber.piston_reverse();
+      }
+      else{
+        Subsystem_climber.piston_Forward();
+      }
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ commands/elevator/Elevator_command.java
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class Elevator_command extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Subsystem_elevator m_subsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem 
+   */
+  public Elevator_command() {
+    subsystem_elevator = Subsystem_elevator.getinstance();
+    
+    addRequirements(Subsystem_elevator);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+
+    Subsystem_elevator.elevatorMovmentControl(RobotContainer.OperatingJoystick.getRawAxis(3));
+     
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+
+    Subsystem_elevator.elevatorMovmentControl(0);
+      
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
